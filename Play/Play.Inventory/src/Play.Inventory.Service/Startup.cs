4787f30a80186f922b7665bbe1f20add7b0f603b
Robert Hardy <hardyrar@gmail.com>
2021-12-29 20:30:03 +0000

    Add exponential retry with Polly

--- Play/Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play/Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -10,6 +10,7 @@ using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 using Polly;
+using Polly.Timeout;
 
 namespace Play.Inventory.Service
 {
@@ -27,7 +28,13 @@ namespace Play.Inventory.Service
         {
             services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
 
+            Random jitterer = new Random();
+            
             services.AddHttpClient<CatalogClient>(client => { client.BaseAddress = new Uri("https://localhost:5001"); })
+                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+                    5,
+                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
+                ))
                 .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
             services.AddControllers();
