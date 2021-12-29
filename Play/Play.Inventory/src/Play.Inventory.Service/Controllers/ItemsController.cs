559ded23211427d67f6c9767013d35bf63c06c19
Robert Hardy <helloarchy@gmail.com>
2021-12-29 18:33:51 +0000

    Update inventory service to use Play.Common, and add endpoints for adding and getting item to users inventory.

---
+++ Play/Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Play.Common;
+using Play.Inventory.Service.Entities;
+
+namespace Play.Inventory.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IRepository<InventoryItem> _itemsRepository;
+
+        public ItemsController(IRepository<InventoryItem> itemsRepository)
+        {
+            _itemsRepository = itemsRepository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
+        {
+            if (userId == Guid.Empty)
+            {
+                return BadRequest();
+            }
+
+            var items = (await _itemsRepository.GetAllAsync(item => item.UserId == userId))
+                .Select(item => item.AsDto());
+
+            return Ok(items);
+        }
+
+        /// <summary>
+        /// Add a new item to the users inventory
+        /// If the user already has this item, increase the quantity, otherwise create it.
+        /// </summary>
+        /// <param name="grantItemsDto">The item to add to the user, or quantity to increase</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(GrantItemsDto grantItemsDto)
+        {
+            var inventoryItem = await _itemsRepository.GetAsync(
+                item => item.UserId == grantItemsDto.CatalogItemId &&
+                        item.CatalogItemId == grantItemsDto.CatalogItemId
+            );
+
+            if (inventoryItem == null)
+            {
+                inventoryItem = new InventoryItem
+                {
+                    CatalogItemId = grantItemsDto.CatalogItemId,
+                    UserId = grantItemsDto.UserId,
+                    Quantity = grantItemsDto.Quantity,
+                    AcquiredDate = DateTimeOffset.UtcNow
+                };
+                
+                await _itemsRepository.CreateAsync(inventoryItem);
+            }
+            else
+            {
+                inventoryItem.Quantity += grantItemsDto.Quantity;
+                await _itemsRepository.UpdateAsync(inventoryItem);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Play/Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Play.Inventory.Service
+{
+    public record GrantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
+    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+}
