2094d984393016f48a4f8284b7c6c9970a7ef81e
Robert Hardy <helloarchy@gmail.com>
2021-12-30 17:43:23 +0000

    Update Common package to include MassTransit message retries

--- Play/Play.Common/src/Play.Common/MassTransit/Extensions.cs
+++ Play/Play.Common/src/Play.Common/MassTransit/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using GreenPipes;
 using MassTransit;
 using MassTransit.Definition;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +24,10 @@ namespace Play.Common.MassTransit
                     var rabbitMqSettings = configuration.GetSection(nameof(RabbitMqSettings)).Get<RabbitMqSettings>();
                     configurator.Host(rabbitMqSettings.Host);
                     configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                    configurator.UseMessageRetry(retryConfigurator =>
+                    {
+                        retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                    });
                 });
             });
 
